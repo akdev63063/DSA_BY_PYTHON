6ecb789feb988cba3d20dbe81e9e52757eab6f1c
Akash_Kumar <akdev63063@gmail.com>
2026-01-10 02:00:46 +0530

    With optimal Solution

--- PrintFactor.py
+++ PrintFactor.py
@@ -1,3 +1,4 @@
+
 n = int(input('Enter any Postive Number : '))
 result = []
 for i in range(1,n+1):
@@ -7,3 +8,22 @@ print(result)
 
 # Time Complexity Tc = O(n)
 # Space Complexity is Sc = O(k), k means which elements store in list 
+"""_______________________Optimal Solution 1________________________"""
+n = int(input('Enter any Postive Number : '))
+result = []
+for i in range(1,(n // 2)+1):
+    if (n % i == 0):
+        result.append(i)
+result.append(n)
+print(result) 
+"""_______________________Optimal Solution 2________________________"""
+from math import sqrt 
+n = int(input('Enter any Postive Number : '))
+result = []
+for i in range(1,int(sqrt(n))+1):
+    if (n % i == 0):
+        result.append(i)
+        result.append(n // i)
+    
+print(sorted(set(result))) 
+
