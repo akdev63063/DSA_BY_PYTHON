04c43564ff6fbb153778ab39de85f73e2bd8bcf8
Akash_Kumar <akdev63063@gmail.com>
2026-01-15 01:51:14 +0530

    All optimal Solution

--- HashingQ2.py
+++ HashingQ2.py
@@ -26,4 +26,35 @@ for num in m1:
     else:
         print(f"{num} {hash_list[num]}")
     
-    
+# Solution 3
+print("\n_______________Solution 3________________________")
+
+n2 = [5,3,2,2,1,5,5,7,5,10]
+m2 = [10,111,1,9,5,67,2]
+
+hash_dic = {}
+
+for num in n2:
+    hash_dic[num] = hash_dic.get(num,0)+1
+
+for num in m2:
+    print(hash_dic.get(num,0))
+
+print("\n_______________Solution 4________________________")
+s = "azyxyyzaaaa"
+q = ["d","a","y","x"]
+hash_list1 = [0] * 26
+
+for ch in s:
+    ascii_val = ord(ch)
+    index = ascii_val-97
+    hash_list1[index] += 1
+
+for ch in q:
+    ascii_val = ord(ch)
+    index = ascii_val-97
+    print(hash_list1[index])
+
+
+
+
